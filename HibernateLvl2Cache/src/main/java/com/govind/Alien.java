c296d8738aaa74f5d05fa7cdd0756d0982392c89
Govind Thakur <govindt7256@gmail.com>
2024-08-23 01:32:45 +0545

    hibernate second level cachce

---
+++ HibernateLvl2Cache/src/main/java/com/govind/Alien.java
@@ -0,0 +1,65 @@
+package com.govind;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+
+@Entity
+@Table(name = "alien")
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Alien { // POJO
+
+    @Id
+    private int aid;
+
+    //    @Transient // if transient, the colum will not be stored.
+    private String aname;
+
+    //    private AlienName aname; // instead of string, using embeddable object.
+    private String color;
+
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+//    public AlienName getAname() {
+//        return aname;
+//    }
+//
+//    public void setAname(AlienName aname) {
+//        this.aname = aname;
+//    }
+
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
